70e0eccbfa8a601121f703daf2674d01cd019476
Gary Hockin <GeeH@Garys-MacBook-Pro.local>
2015-10-04 09:31:37 +0100

    Boom chukka waa waa

---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE HTML>
+<!--
+	Strongly Typed by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>Sustain - Helping the dev community to thrive</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <!--[if lte IE 8]>
+    <script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css"/>
+    <!--[if lte IE 8]>
+    <link rel="stylesheet" href="assets/css/ie8.css"/><![endif]-->
+</head>
+<body class="homepage">
+<div id="page-wrapper">
+
+    <!-- Header -->
+    <div id="header-wrapper">
+        <div id="header" class="container">
+
+            <section id="features" class="container">
+
+                <div class="row">
+                    <div class="4u 12u(mobile)">
+
+
+                    </div>
+                    <div class="4u 12u(mobile)">
+
+                        <!-- Feature -->
+                        <section>
+                            <a class="image no-border"><img src="images/sustain_logo_vertical_brown.png" alt=""/></a>
+                            <header>
+                                <h3>Helping the dev community thrive.</h3>
+                            </header>
+                        </section>
+
+                    </div>
+                    <div class="4u 12u(mobile)">
+
+
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        <!-- Nav -->
+        <nav id="nav">
+            <ul>
+                <li><a class="icon fa-question-circle" href="#what"><span>What on Earth?</span></a></li>
+                <li><a class="icon fa-heart" href="#people"><span>I'd Like to Go!</span></a></li>
+                <li><a class="icon fa-heart" href="#"><span>I'd Like to Help!</span></a></li>
+                <li><a class="icon fa-twitter" href="https://twitter.com/sustaincommunit"><span>@sustaincommunit</span></a>
+                </li>
+                <li><a class="icon fa-beer" href="#"><span>Other Help?</span></a></li>
+
+            </ul>
+        </nav>
+
+    </div>
+</div>
+
+<!-- Features -->
+<div id="features-wrapper">
+    <section id="features" class="container">
+
+        <div class="row">
+            <!-- Feature -->
+            <section id="banner" class="container">
+                <a name="what no-border">
+                    <img src="images/sustain_logo_leaf.png" class="centered 1u 3u(mobile)" alt=""/>
+                </a>
+
+                <p>We help the development community to <strong>thrive</strong> by helping to get
+                    <strong>deserving</strong> people to <strong>amazing</strong> events</p>
+            </section>
+            <p>
+                Things happen in life, and you can't always give your career the love and care it deserves. We've all
+                found an amazing conference or training course coming up just at the right time. There's bill to pay,
+                and while you know that the amazing event you would love to go to is a solid investment in your future
+                income, you just can't quite afford it right now.
+            </p>
+
+            <p>
+                <strong>Sustain</strong> can help.
+            </p>
+
+            <p>
+                People in the development community are amazing. They are some of the
+                most generous and incredible individuals in any walks of life, and believe it or not, they are happy to
+                help YOU. Sometimes people have spare tickets, and they are delighted to donate a conference ticket to
+                somebody they know will get good use out of it. Sometimes they are "paying it forward", they're giving
+                someone else a leg up financially, because someone gave them a leg up in the past. And sometimes they
+                are doing it out of the goodness of their hearts.
+            </p>
+        </div>
+
+    </section>
+</div>
+
+<div id="features-wrapper">
+    <section id="features" class="container">
+
+        <div class="row">
+            <!-- Feature -->
+            <section id="banner" class="container">
+                <a name="people">
+                    <img src="images/sustain_logo_leaf.png" class="centered 1u 3u(mobile)" alt=""/>
+                </a>
+
+                <p>I'd really like to get to event <strong>X</strong>, is this for real?</p>
+
+            </section>
+            <p>
+                Things happen in life, and you can't always give your career the love and care it deserves. We've all
+                found an amazing conference or training course coming up just at the right time. There's bill to pay,
+                and while you know that the amazing event you would love to go to is a solid investment in your future
+                income, you just can't quite afford it right now.
+            </p>
+
+            <p>
+                <strong>Sustain</strong> can help.
+            </p>
+
+            <p>
+                People in the development community are amazing. They are some of the
+                most generous and incredible individuals in any walks of life, and believe it or not, they are happy to
+                help YOU. Sometimes people have spare tickets, and they are delighted to donate a conference ticket to
+                somebody they know will get good use out of it. Sometimes they are "paying it forward", they're giving
+                someone else a leg up financially, because someone gave them a leg up in the past. And sometimes they
+                are doing it out of the goodness of their hearts.
+            </p>
+        </div>
+
+    </section>
+</div>
+
+
+<!-- Main -->
+
+<!-- Footer -->
+<div id="footer-wrapper">
+    <div id="footer" class="container">
+        <header>
+            <h2>Questions or comments? <strong>Get in touch:</strong></h2>
+        </header>
+        <div class="row">
+            <div class="6u 12u(mobile)">
+                <section>
+                    <form method="post" action="#">
+                        <div class="row 50%">
+                            <div class="6u 12u(mobile)">
+                                <input name="name" placeholder="Name" type="text"/>
+                            </div>
+                            <div class="6u 12u(mobile)">
+                                <input name="email" placeholder="Email" type="text"/>
+                            </div>
+                        </div>
+                        <div class="row 50%">
+                            <div class="12u">
+                                <textarea name="message" placeholder="Message"></textarea>
+                            </div>
+                        </div>
+                        <div class="row 50%">
+                            <div class="12u">
+                                <a href="#" class="form-button-submit button icon fa-envelope">Send Message</a>
+                            </div>
+                        </div>
+                    </form>
+                </section>
+            </div>
+            <div class="6u 12u(mobile)">
+                <section>
+                    <p>Erat lorem ipsum veroeros consequat magna tempus lorem ipsum consequat Phaselamet
+                        mollis tortor congue. Sed quis mauris sit amet magna accumsan tristique. Curabitur
+                        leo nibh, rutrum eu malesuada.</p>
+
+                    <div class="row">
+                        <div class="6u 12u(mobile)">
+                            <ul class="icons">
+                                <li class="icon fa-home">
+                                    1234 Somewhere Road<br/>
+                                    Nashville, TN 00000<br/>
+                                    USA
+                                </li>
+                                <li class="icon fa-phone">
+                                    (000) 000-0000
+                                </li>
+                                <li class="icon fa-envelope">
+                                    <a href="#">info@untitled.tld</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="6u 12u(mobile)">
+                            <ul class="icons">
+                                <li class="icon fa-twitter">
+                                    <a href="#">@untitled-tld</a>
+                                </li>
+                                <li class="icon fa-instagram">
+                                    <a href="#">instagram.com/untitled-tld</a>
+                                </li>
+                                <li class="icon fa-dribbble">
+                                    <a href="#">dribbble.com/untitled-tld</a>
+                                </li>
+                                <li class="icon fa-facebook">
+                                    <a href="#">facebook.com/untitled-tld</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+    <div id="copyright" class="container">
+        <ul class="links">
+            <li>&copy; Untitled. All rights reserved.</li>
+            <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+        </ul>
+    </div>
+</div>
+
+</div>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.dropotron.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/skel-viewport.min.js"></script>
+<script src="assets/js/util.js"></script>
+<!--[if lte IE 8]>
+<script src="assets/js/ie/respond.min.js"></script><![endif]-->
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
